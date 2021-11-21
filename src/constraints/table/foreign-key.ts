4d58084f34473edea680c9fc7779221be29ed495
joshuamaciak <jtm97@pitt.edu>
2021-11-20 23:02:55 -0500

    move constraint creation into constraint packages (#7)

--- src/constraints/table/foreign-key.ts
+++ src/constraints/table/foreign-key.ts
@@ -1,4 +1,5 @@
-import {TableConstraint} from './table-constraint';
+import {ForeignKeyConfig} from '../../decorators/entity';
+import {TableConstraint, TableConstraintType} from './table-constraint';
 
 /**
  *  A FOREIGN KEY table constraint of the form:
@@ -18,4 +19,15 @@ export function convertForeignKeyTableConstraintToSql(
   const columnNames = constraint.columnNames.join(',');
   const foreignColumNames = constraint.foreignColumnNames.join(',');
   return `${constraint.type} (${columnNames}) REFERENCES ${constraint.foreignTableName} (${foreignColumNames})`;
+}
+
+export function createForeignKeyTableConstraint(
+  config: ForeignKeyConfig,
+): ForeignKeyTableConstraint {
+  return {
+    type: TableConstraintType.FOREIGN_KEY,
+    columnNames: config.columnNames,
+    foreignTableName: '', // TODO: get foreign table name from config.foreignEntity
+    foreignColumnNames: config.foreignColumnNames,
+  };
 }
